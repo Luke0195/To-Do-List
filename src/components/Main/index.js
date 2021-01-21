bade843c64735626834f7841d0b850a4777e3303
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-21 10:36:31 -0300

    feat: eslitization of tasks

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import './styles.css';
 
 // Form
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus, FaTrash, FaEdit } from 'react-icons/fa';
 
 export default class Main extends Component {
  state ={
    novaTarefa: '',
+   tarefas: [
+     'Acordar',
+     'Tomar Café',
+     'Escovar os dentes',
+     'Ir Trabalhar',
+     'Estudar',
+     'Ir dormir',
+   ],
  }
 
  handleChange = (event) => {
@@ -16,26 +24,41 @@ export default class Main extends Component {
  }
 
  render() {
-   const { novaTarefa } = this.state;
+   const { novaTarefa, tarefas } = this.state;
    return (
-     <div className="main">
-       <p>
+     <>
+       <div className="main">
+         <p>
 
-         {novaTarefa}
-       </p>
-       <h1> Lista de Tarefas</h1>
-       <form className="form">
-         <input
-           type="text"
-           onChange={this.handleChange}
-           value={novaTarefa}
-         />
-         <button type="submit">
-           <FaPlus size={16} />
-         </button>
-       </form>
-     </div>
+           {novaTarefa}
+         </p>
+         <h1> Lista de Tarefas</h1>
+         <form className="form">
+           <input
+             type="text"
+             onChange={this.handleChange}
+             value={novaTarefa}
+           />
+           <button type="submit">
+             <FaPlus size={16} />
+           </button>
+         </form>
 
+         <ul className="list">
+           {tarefas.map((tarefa) => (
+             <li key={tarefa}>
+               <strong>{tarefa}</strong>
+               <div className="icons">
+                 <FaEdit size={16} color="#c53f53" />
+                 <FaTrash size={16} color="#34632e" />
+
+               </div>
+             </li>
+           ))}
+         </ul>
+       </div>
+
+     </>
    );
  }
 }
